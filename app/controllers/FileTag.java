f0e73660795b43c09cdfdb79e95bb9cbd680c249
Bruno Gonçalves <victany5@hotmail.com>
2015-11-19 10:20:59 +0000

    start addTag

---
+++ app/controllers/FileTag.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+/**
+ * Created by user on 19-11-2015.
+ */
+public class FileTag extends Controller {
+
+    public Result addTag() {
+
+    }
+
+    public Result getTagById(Long id){
+
+    }
+
+    public Result getAllTags(){
+
+    }
+
+}
+
+
