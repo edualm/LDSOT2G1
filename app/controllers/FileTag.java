ec7b2763c7608e340896154218acc9925534d845
Bruno Gonçalves <victany5@hotmail.com>
2015-11-19 12:09:04 +0000

    ....

--- app/controllers/FileTag.java
+++ app/controllers/FileTag.java
@@ -3,6 +3,7 @@ package controllers;
 import com.avaje.ebean.Model;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import models.Tag;
+import play.data.DynamicForm;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -16,13 +17,35 @@ public class FileTag extends Controller {
     public static Model.Finder<Long, models.Tag> tags = new Model.Finder(Long.class, models.Tag.class);
 
     public Result addTag() {
+        DynamicForm dynamicForm = new DynamicForm().bindFromRequest();
+
+        try {
+            String name = dynamicForm.get("nome");
+
+            if (name != null) {
+                Tag query = tags.where().ilike("nome", "name").findUnique();
+
+                if (query == null) {
+                    Tag t =  new Tag(name);
+                    t.save();
+
+                    return ok(Json.toJson(t));
+                }
+            }
+
+        } catch (Exception e) {
+            ObjectNode json = Json.newObject();
+            json.put("result", "error");
+            json.put("excecao", e.getMessage());
+            return badRequest(json);
+        }
+
 
     }
 
     public Result getTagById(Long id){
 
         ObjectNode response = Json.newObject();
-
 
         if(id == 0)
             return badRequest("Wrong File ID");
