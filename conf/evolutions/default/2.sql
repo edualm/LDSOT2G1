afb0c107b34d211dfa797a07f12fe7a3a8d537e3
damboscolo <danielehidalgo@gmail.com>
2015-11-26 11:22:11 +0000

    Corrigido sql

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -20,7 +20,7 @@ Date: 2015-11-18 11:58:57
 -- Foreign Key structure for table "public"."comentario"
 -- ----------------------------
 
-ALTER TABLE "public"."comentario" DROP CONSTRAINT comentario_projecto_id_fkey;
+ALTER TABLE "public"."comentario" DROP CONSTRAINT "comentario_projecto_id_fkey";
 
 ALTER TABLE "public"."comentario" ADD FOREIGN KEY ("projecto_id") REFERENCES "public"."projecto" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
@@ -28,24 +28,8 @@ ALTER TABLE "public"."comentario" ADD FOREIGN KEY ("projecto_id") REFERENCES "pu
 -- Foreign Key structure for table "public"."versaoprojecto"
 -- ----------------------------
 
-ALTER TABLE "public"."versaoprojecto" DROP CONSTRAINT versaoprojecto_projecto_id_fkey;
+ALTER TABLE "public"."versaoprojecto" DROP CONSTRAINT "versaoprojecto_projecto_id_fkey";
 
 ALTER TABLE "public"."versaoprojecto" ADD FOREIGN KEY ("projecto_id") REFERENCES "public"."projecto" ("id") ON DELETE CASCADE ON UPDATE NO ACTION;
 
 # --- !Downs
-DROP SEQUENCE IF EXISTS "public"."Comentario_id_seq";
-DROP SEQUENCE IF EXISTS "public"."Componente_id_seq";
-DROP SEQUENCE IF EXISTS "public"."Ficheiro_id_seq";
-DROP SEQUENCE IF EXISTS "public"."ligacao_id_seq";
-DROP SEQUENCE IF EXISTS "public"."Projecto_id_seq";
-DROP SEQUENCE IF EXISTS "public"."Tag_id_seq";
-DROP SEQUENCE IF EXISTS "public"."VersaoProjecto_id_seq";
-DROP TABLE IF EXISTS "public"."comentario";
-DROP TABLE IF EXISTS "public"."componente";
-DROP TABLE IF EXISTS "public"."ficheiro";
-DROP TABLE IF EXISTS "public"."ficheiro_tag";
-DROP TABLE IF EXISTS "public"."ligacao";
-DROP TABLE IF EXISTS "public"."tag";
-DROP TABLE IF EXISTS "public"."tipo";
-DROP TABLE IF EXISTS "public"."versaoprojecto";
-DROP TABLE IF EXISTS "public"."versaoprojecto_componente";
