bda8d99ae8901f1e1991b35271a429662c28fb63
Eduardo Almeida <me@edr.io>
2015-12-03 17:27:51 +0000

    Maybe now.

--- app/controllers/Editor.java
+++ app/controllers/Editor.java
@@ -1,7 +1,15 @@
 package controllers;
 
+import play.mvc.Controller;
+import play.mvc.Result;
+
+import views.html.editor;
+
 /**
  * Created by MegaEduX on 03/12/15.
  */
-public class Editor {
+public class Editor extends Controller {
+    public Result showTestPage() {
+        return ok(editor.render());
+    }
 }
