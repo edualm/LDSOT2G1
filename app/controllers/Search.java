5d7dcebb4dba226fc99e9e32637edb6f86198af8
Bruno Gonçalves <victany5@hotmail.com>
2016-01-04 16:50:31 +0000

    search changes done

--- app/controllers/Search.java
+++ app/controllers/Search.java
@@ -2,17 +2,17 @@ package controllers;
 
 import com.avaje.ebean.Ebean;
 import com.avaje.ebean.Expr;
+
+import java.util.ArrayList;
 import java.util.List;
 import models.Projecto;
+import models.Tag;
+import models.VersaoProjecto;
 import play.mvc.Result;
 import play.data.DynamicForm;
 import play.mvc.Controller;
 import utilities.AuthManager;
 import views.html.search;
-
-/**
- * Created by MegaEduX on 23/12/15.
- */
 
 public class Search extends Controller {
     public Result searchResults() {
@@ -25,9 +25,8 @@ public class Search extends Controller {
 
         List<Projecto> res = Ebean.find(Projecto.class).where().or(Expr.icontains("nome", query), Expr.icontains("descricao", query)).findList();
 
-        List<Projecto> tagsProject =
-                Ebean.find(Projecto.class).where().or(Expr.icontains("nome", query), Expr.icontains("descricao", query)).findList();
+        List<Tag> tag = Ebean.find(Tag.class).where().eq("nome", query).findList();
 
-        return ok(search.render(res, AuthManager.authCheck(session(), form)));
+        return ok(search.render(query, res, tag, AuthManager.authCheck(session(), form)));
     }
 }
