804e3dc3ffae528abf11a181c3d5d3461b6a4620
Bruno Gonçalves <victany5@hotmail.com>
2016-01-05 16:26:27 +0000

    ficheiros projeto

--- app/controllers/Project.java
+++ app/controllers/Project.java
@@ -78,7 +78,7 @@ public class Project extends Controller {
 
             ArrayList<VersaoProjecto> vps = new ArrayList<>();
 
-            return ok(project.render(p, true, vs, vs.componentes, vps, p.tags));
+            return ok(project.render(p, true, vs, vs.componentes, vps, p.tags, p.ficheiros));
         } else {
             return redirect(AuthManager.AuthServer_URI + "?callback=" + AuthManager.getServerURL(request()));
         }
@@ -247,7 +247,7 @@ public class Project extends Controller {
 
         Collections.reverse(vps);
 
-        return ok(project.render(p, true, ver, ver.componentes, vps, p.tags));
+        return ok(project.render(p, true, ver, ver.componentes, vps, p.tags, p.ficheiros));
     }
 
     public Result getProjectoById(Long id){
@@ -262,7 +262,7 @@ public class Project extends Controller {
 
         Collections.reverse(vps);
 
-        return ok(project.render(p, true, ver, ver.componentes, vps, p.tags));
+        return ok(project.render(p, true, ver, ver.componentes, vps, p.tags, p.ficheiros));
     }
     
     public Result getAllProjectos() {
