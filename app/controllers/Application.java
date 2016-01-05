1e918d968f40d744bd6890301ec6ae6d954a3aae
Rita Faria <ritafariavr@gmail.com>
2016-01-05 17:01:10 +0000

    Homepage

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,46 +2,51 @@ package controllers;
 
 import com.avaje.ebean.Expr;
 import com.avaje.ebean.Model;
+import models.Projecto;
 import models.Sessions;
-import play.*;
 import play.data.DynamicForm;
-import play.mvc.*;
-
+import play.mvc.Controller;
+import play.mvc.Result;
 import utilities.AuthManager;
-import views.html.*;
+import views.html.index;
 
 import java.util.List;
 
 public class Application extends Controller {
-    
+
     public static Model.Finder<Long, Sessions> sessions = new Model.Finder(String.class, Sessions.class);
-    
-    
+
+
     public Result index() {
-        return ok(index.render("Your new application is ready.", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest())));
+
+        List<Projecto> p = Project.projectos.where().order("id desc").setMaxRows(10).findList();
+
+        return ok(index.render("Your new application is ready.", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest()), p));
     }
-    
+
     public Result logout() {
+
+        List<Projecto> p = Project.projectos.all();
         try {
             String sess = session("jwt");
-            
+
             if(sess != null){
                 if(AuthManager.currentUsername(sess) != null){
                     List<Sessions> query = sessions.query().where().and(Expr.eq("token",sess),(Expr.eq("username", AuthManager.currentUsername(sess)))).findList();
                     if(query.size() > 0){
                         query.get(0).delete();
                         session().clear();
-                        return ok(index.render("Logged out.", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest())));
+                        return ok(index.render("Logged out.", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest()), p));
                     }
                 }
             }
-            
+
             session().clear();
-            
-            return ok(index.render("Logged out", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest())));
+
+            return ok(index.render("Logged out", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest()), p));
         } catch (Exception e){
             session().clear();
-            return ok(index.render("Logged out", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest())));
+            return ok(index.render("Logged out", AuthManager.authCheck(session(), new DynamicForm().bindFromRequest()), p));
         }
     }
 }
